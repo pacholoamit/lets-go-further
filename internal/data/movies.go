e9d4302689b65da51e0aea8e9a75fc299024a5c7
pacholoamit <pacholoamit.tech@gmail.com>
2022-07-24 03:03:33 +0800

    Implement custom MarshalJSON method for Runtime type

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -6,7 +6,7 @@ type Movie struct {
 	ID        int64     `json:"id"`
 	Title     string    `json:"title"`
 	Year      int32     `json:"year,omitempty"`
-	Runtime   int32     `json:"runtime,omitempty,string"`
+	Runtime   Runtime   `json:"runtime,omitempty,string"`
 	Genres    []string  `json:"genres,omitempty"`
 	Version   int32     `json:"version"`
 	CreatedAt time.Time `json:"-"`
