49c414a3cb7e59c97a86833a1b0dd28d92f2b770
pacholoamit <pacholoamit.tech@gmail.com>
2022-07-25 02:49:03 +0800

    Implement querying of movies on showMovieHandler

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/pacholoamit/lets-go-further/internal/data"
 	"github.com/pacholoamit/lets-go-further/internal/validator"
@@ -64,13 +64,16 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	movie := data.Movie{
-		ID:        id,
-		CreatedAt: time.Now(),
-		Title:     "Star Wars",
-		Runtime:   102,
-		Genres:    []string{"Sci-fi", "Action"},
-		Version:   1,
+	movie, err := app.models.Movies.Get(id)
+
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
 	}
 
 	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
